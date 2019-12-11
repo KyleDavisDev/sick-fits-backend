aff6c87775a79f8030d22693e84b7550733a8c55
Kyle Bonar <kylewb2011@gmail.com>
2019-12-10 19:49:56 -0600

    updateItem mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -5,6 +5,22 @@ const Mutation = {
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
 
     return item;
+  },
+  updateItem: async function(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const updates = { ...args };
+
+    delete updates.id;
+
+    // run update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: { id: args.id }
+      },
+      info
+    );
   }
 };
 
