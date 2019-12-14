520362d195500c2b3bfe6c0da77f0c2c4fe30d0f
Kyle Bonar <kylewb2011@gmail.com>
2019-12-14 15:51:09 -0600

    Added deleteItem mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -14,13 +14,24 @@ const Mutation = {
     delete updates.id;
 
     // run update method
-    return ctx.db.mutation.updateItem(
+    return await ctx.db.mutation.updateItem(
       {
         data: updates,
         where: { id: args.id }
       },
       info
     );
+  },
+  deleteItem: async function(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const where = { id: args.id };
+    // 1. Find the item
+    const item = await ctx.db.query.item({ where }, `{ id title}`);
+    // 2. Check permissions
+
+    // 3. Delete it
+    return await ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
