88d83b99415c90170832e59a43c1c821ac5c7290
Kyle Bonar <kylewb2011@gmail.com>
2019-12-28 11:28:00 -0600

    Added comment

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const db = require("./db");
 
 const server = createServer();
 
+// Transform ugly cookies into nice cookies
 server.express.use(cookieParser());
 //TODO: Use express middleware to populate the current user
 
