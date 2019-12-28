4a41ce438e771b38096d9ba11a6ae8a414f875db
Kyle Bonar <kylewb2011@gmail.com>
2019-12-28 11:46:56 -0600

    New signUp method

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const Mutation = {
   createItem: async function(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -32,6 +35,31 @@ const Mutation = {
 
     // 3. Delete it
     return await ctx.db.mutation.deleteItem({ where }, info);
+  },
+  signUp: async function(parent, args, ctx, info) {
+    // First.Last@email.com ==> first.last@email.com
+    args.email = args.email.toLowerCase();
+
+    // Hash password
+    const password = await bcrypt.hash(args.password, 10);
+
+    // create user
+    const user = await ctx.db.mutation.createUser(
+      { data: { ...args, password, permissions: { set: ["USER"] } } },
+      info
+    );
+
+    // create JWT
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    // set jwt to cookie
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 30 // 30 day token
+    });
+
+    // Finally return
+    return user;
   }
 };
 
